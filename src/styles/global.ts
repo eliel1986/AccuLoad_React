fc97b447dc003bf66a61ace0faba5ae4ba978cb9
eliel1986 <eliel_1986@hotmail.com>
2022-11-02 21:21:49 -0300

    concluido resolução 400px

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -172,7 +172,7 @@ body {
     }
 
     div.display {
-        width: 280px;
+        width: 220px;
         background-color: #639033;
         display: flex;
         border-radius: 10px;
@@ -184,43 +184,43 @@ body {
     div.header {
         position: absolute;
         margin-top: 10px;
-        font-size: 18px;
+        font-size: 14px;
     }
 
     div.content {
         position: absolute;
         margin-top: 40px;
-        font-size: 18px;
+        font-size: 14px;
     }
 
     div.footer {
         position: absolute;
-        margin-top: 78px;
-        font-size: 14px;
+        margin-top: 80px;
+        font-size: 12px;
     }
 
     div.upperLeft {
         width: 120px;
         position: absolute;
-        margin-top: 5px;
-        margin-left: -155px;
-        font-size: 14px;
+        margin-top: 3px;
+        margin-left: -130px;
+        font-size: 12px;
         text-align: center;
     }
 
     div.lowerLeft {
         width: 120px;
         position: absolute;
-        margin-top: 44px;
-        margin-left: -155px;
-        font-size: 14px;
+        margin-top: 48px;
+        margin-left: -130px;
+        font-size: 12px;
         text-align: center;
     }
 
     div.percent {
         width: 60px;
         position: absolute;
-        margin-top: 46px;
+        margin-top: 58px;
         margin-left: -25px;
         font-size: 14px;
         text-align: center;
@@ -231,26 +231,26 @@ body {
         width: 130px;
         position: absolute;
         margin-top: 2px;
-        margin-left: 34px;
-        font-size: 12px;
+        margin-left: 2px;
+        font-size: 10px;
         text-align: center;
     }
 
     div.middleRight {
         width: 130px;
         position: absolute;
-        margin-top: 32px;
-        margin-left: 34px;
-        font-size: 12px;
+        margin-top: 26px;
+        margin-left: 2px;
+        font-size: 10px;
         text-align: center;
     }
 
     div.lowerRight {
         width: 130px;
         position: absolute;
-        margin-top: 62px;
-        margin-left: 34px;
-        font-size: 12px;
+        margin-top: 52px;
+        margin-left: 2px;
+        font-size: 10px;
         text-align: center;
     }
 
@@ -262,7 +262,7 @@ body {
         padding-top: 20px;
         margin-top: 28px;
         width: 285px;
-        height: 250px;
+        height: 238px;
         background-color: black;
         border: 2px solid #555;
         border-radius: 20px;
@@ -274,11 +274,11 @@ body {
         justify-content: center;
     }
         button {
-        width: 52px;
-        height: 44px;
+        width: 42px;
+        height: 38px;
         background-color: white;
         color: black;
-        font-size: 18px;
+        font-size: 14px;
         font-style: italic;
         display: flex;
         justify-content: center;
